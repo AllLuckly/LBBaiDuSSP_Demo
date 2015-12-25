2b23674e1062b001e1fa77502d1e62aefefe236b
_Biosn <435844159@qq.com>
2015-12-25 23:59:24 +0800

    Initial Commit

---
+++ LBBaiDuSSP_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBBaiDuSSP_Demo
+//
+//  Created by bison on 15/12/25.
+//  Copyright © 2015年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBBaiDuSSP_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBBaiDuSSP_Demo
+//
+//  Created by bison on 15/12/25.
+//  Copyright © 2015年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
